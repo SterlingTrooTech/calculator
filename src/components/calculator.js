fbe5b81e21e034eed6d43d62708e995abaec3d4a
SpGoku <sterling.parmar@trootech.com>
2022-08-04 17:07:51 +0530

    calculator.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,26 +1,18 @@
 /* eslint-disable */
 import React, {useState} from 'react';
 import './calculator.css'
-import evaluate from './evaluate';
 import { CalcUi } from './numPad';
 
 const Calculator = () => {
   const [val, setVal] = useState('0');
   const [decimal, setDecimal] = useState(false);
 
-  const [calc, setCalc] = useState(0);
-  const [num1, setNum1] = useState(0);
-  const [op, setOp] = useState();
-  const [equal, setEqual] = useState(0)
-
   const numClick = (e) => {
     
     if (val == '0') {
       setVal(e.target.value)
-      setNum1(e.target.value)
     } else {
       setVal(val + (e.target.value))
-      setNum1(num1 + (e.target.value))
     }
   }
 
@@ -37,28 +29,15 @@ const Calculator = () => {
       setVal(val + (e.target.value))
     }
     setDecimal(false)
-    setCalc(num1)
-
-    try{
-
-      setCalc(evaluate(op, calc, num1))
-
-    } finally {
-      setOp(e.target.value)
-      setNum1(0)
-    }
   }
 
   const decimalClick = (e) => {
     setVal(val + e.target.value)
-    setNum1(num1 + e.target.value)
     setDecimal(true)
   }
 
   const clearVals = () => {
     setVal(0)
-    setCalc(0)
-    setEqual(0)
     setDecimal(false)
   }
 
@@ -73,26 +52,10 @@ const Calculator = () => {
         setVal(val?.substring(0, val?.length - 1))
       }
     }
-    let c = calc.toString()
-    if(c === 0 || c.length <= 1){
-      setCalc(0)
-      setEqual(0)
-    } else {
-      if(isNaN(c?.charAt(c?.length - 1))){
-        setCalc(parseInt(c?.substring(0, c?.length - 1)))
-        setDecimal(false)
-      } else {
-        setCalc(parseInt(c?.substring(0, c?.length - 1)))
-      }
-    }
   }
 
   const calculate = () => {
-    setNum1(evaluate(op, calc, num1))
-    setCalc(evaluate(op, calc, num1))
     setVal((eval(val)).toString())
-    setEqual(1)
-    setOp()
     setDecimal(false)
   }
 
@@ -108,7 +71,6 @@ const Calculator = () => {
         decimal = {decimal}
         val = {val}
       />
-      {equal? <div className='display2'>{calc}</div>: <></>}
     </>
   )
 }
